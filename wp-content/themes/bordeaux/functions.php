b4771551ea52f6dff9587b78f12a266aa21d8514
Rémy <remdub@live.fr>
2017-05-02 16:02:04 +0200

    Add chosen theme and it's child

---
+++ wp-content/themes/bordeaux/functions.php
@@ -0,0 +1,13 @@
+<?php
+function test_enqueue_styles() {
+
+    $parent_style = 'wellington-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'test_enqueue_styles' );
+?>
