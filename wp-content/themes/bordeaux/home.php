cfd88eaef418b29940c6d623e56a0cf7db7c2545
solene <solenantoine@gmail.com>
2017-05-10 17:13:04 +0200

    remake script home AGAIN

--- wp-content/themes/bordeaux/home.php
+++ wp-content/themes/bordeaux/home.php
@@ -32,27 +32,58 @@ get_header(); ?>
             <div id="post-wrapper" class="post-wrapper clearfix">
 
 				<?php
-                $my_query = new WP_Query('showposts=1');
-                while ( $my_query->have_posts() ) : $my_query->the_post();
-                    $do_not_duplicate = $post->ID;
-                    get_template_part( 'template-parts/content' );
-                endwhile;
+                $args = array(
+                                'orderby' => 'date',
+                                'posts_per_page' => 1
+                );
+                $firstpost = get_posts($args);
+                    foreach ($firstpost as $post) {
+                        setup_postdata( $post );
+                            $do_not_duplicate = $post->ID;
+                            get_template_part( 'template-parts/content' );
+                    }
+
+                // $categories = get_categories( array( 'orderby' => 'name', 'order' => 'ASC' ) );
+                // foreach( $categories as $category ) {
+                //     echo '<h2>'.$category->name.'</h2>';
+                //         $args = array( 'posts_per_page' => -1, 'order'=> 'ASC', 'orderby' => 'title', 'category' => $category->cat_ID );
+                //         $postslist = get_posts( $args );
+                //             foreach ( $postslist as $post ) :
+                // $my_query = new WP_Query('showposts=1');
+                // while ( $my_query->have_posts() ) : $my_query->the_post();
+                // endwhile;
                 ?>
             </div>
             <h3>ACTUALITES</h3>
             <div id="post-wrapper2" class="post-wrapper clearfix">
                 <?php
-                    query_posts('showposts=4');
-                        while ( have_posts() ) : the_post();
-                            if ( $post->ID != $do_not_duplicate && 'post' === get_post_type() ) :
-                                get_template_part( 'template-parts/content' );
-                            endif;
-                        endwhile; ?>
+                $args = array(
+                    'orderby' => 'date',
+                    'posts_per_page' => 4,
+                    'category' => 'actualites'
+                );
+                $secposts = get_posts($args);
+                foreach ($secposts as $post) {
+                    setup_postdata( $post );
+                        if ( $post->ID != $do_not_duplicate && 'post' === get_post_type() ) :
+                            get_template_part( 'template-parts/content' );
+                        endif;
+                }
+                    ?>
 
     			</div>
+                <div class="">
+                    <iframe src="//v.calameo.com/library/?type=account&id=1480121&rows=1&sortBy=latestIssue&theme=any&bgColor=ebebeb&thumbSize=normal&showTitle=false&showShadow=true&showGloss=false&showInfo=no&linkTo=embed" height="185" width="100%" frameborder="0" allowfullscreen allowtransparency></iframe>
+                </div>
+                <div class="middle">
+                    <div class="video">
+                        <h3>VIDEOS</h3>
+                        <iframe id="dm_jukebox_iframe" allowfullscreen="true" allowtransparency="true" style="overflow:hidden; margin:0; padding:0; width: 100%; height: 302px;" marginwidth="0" marginheight="0" src="http://www.dailymotion.com/widget/jukebox?list[]=%2Fplaylist%2Fx2a4hy_villedebordeaux_a-la-une%2F1&&autoplay=0&mute=0" width="100%" frameborder="0" align="middle">
+                    </div>
+                    <div class="politique">
 
-                	<iframe src="//v.calameo.com/library/?type=account&id=1480121&rows=1&sortBy=latestIssue&theme=any&bgColor=ebebeb&thumbSize=normal&showTitle=false&showShadow=true&showGloss=false&showInfo=no&linkTo=embed" height="185" width="100%" frameborder="0" allowfullscreen allowtransparency></iframe>
-                    
+                    </div>
+                </div>
 
 		<?php
 		else :
